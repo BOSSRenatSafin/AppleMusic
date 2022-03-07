81790179bc3e2691c3e003b7829dce4b2337175a
Ренат <BOSS.Renat.Safin@gmail.com>
2022-03-07 22:32:47 +0300

    create media library.

---
+++ AppleMusic/MiniPlayer.swift
@@ -0,0 +1,20 @@
+//
+//  MiniPlayer.swift
+//  AppleMusic
+//
+//  Created by Ренат Сафин on 07.03.2022.
+//
+
+import SwiftUI
+
+struct MiniPlayer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MiniPlayer_Previews: PreviewProvider {
+    static var previews: some View {
+        MiniPlayer()
+    }
+}
