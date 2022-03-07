5aca2b2d1f13bf819a84487df1656d25d49069f5
Ренат <BOSS.Renat.Safin@gmail.com>
2022-03-07 19:53:04 +0300

    Initial Commit

---
+++ AppleMusic/AppleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicApp.swift
+//  AppleMusic
+//
+//  Created by Ренат Сафин on 07.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
