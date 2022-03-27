dc6336d5b77b51e6380af7aa5e73860465f0206a
Ренат <BOSS.Renat.Safin@gmail.com>
2022-03-27 23:55:21 +0300

    Added сhanging the position of a cell and selection.

--- AppleMusic/MediaEditing.swift
+++ AppleMusic/MediaEditing.swift
@@ -13,22 +13,25 @@ private struct Cell: Identifiable {
     var id: String { name }
 }
 
-private let cells: [Cell] = [
-    Cell(name: "Плейлисты", nameImage: "music.note.list"),
-    Cell(name: "Артисты", nameImage: "music.mic"),
-    Cell(name: "Альбомы", nameImage: "square.stack"),
-    Cell(name: "Песни", nameImage: "music.note"),
-    Cell(name: "Телешоу и фильмы", nameImage: "tv"),
-    Cell(name: "Видеоклипы", nameImage: "music.note.tv"),
-    Cell(name: "Жанры", nameImage: "guitars"),
-    Cell(name: "Сборники", nameImage: "person.2.crop.square.stack"),
-    Cell(name: "Авторы", nameImage: "music.quarternote.3"),
-    Cell(name: "Загружено", nameImage: "arrow.down.circle"),
-    Cell(name: "Домашняя коллекция", nameImage: "music.note.house")]
-
 struct MediaEditing: View {
+    
+    @State private var multiSelection = Set<UUID>()
+    
+    @State private var cells: [Cell] = [
+        Cell(name: "Плейлисты", nameImage: "music.note.list"),
+        Cell(name: "Артисты", nameImage: "music.mic"),
+        Cell(name: "Альбомы", nameImage: "square.stack"),
+        Cell(name: "Песни", nameImage: "music.note"),
+        Cell(name: "Телешоу и фильмы", nameImage: "tv"),
+        Cell(name: "Видеоклипы", nameImage: "music.note.tv"),
+        Cell(name: "Жанры", nameImage: "guitars"),
+        Cell(name: "Сборники", nameImage: "person.2.crop.square.stack"),
+        Cell(name: "Авторы", nameImage: "music.quarternote.3"),
+        Cell(name: "Загружено", nameImage: "arrow.down.circle"),
+        Cell(name: "Домашняя коллекция", nameImage: "music.note.house")]
+    
     var body: some View {
-        List {
+        List(selection: $multiSelection) {
             ForEach (cells) { cells in
                 HStack{
                     Image(systemName: cells.nameImage)
@@ -36,8 +39,16 @@ struct MediaEditing: View {
                     Text(cells.name)
                 }
             }
+            .onMove(perform: move)
         }
         .listStyle(.inset)
+    }
+    
+    //MARK: Move Row
+    func move(from source: IndexSet, to destinstion: Int) {
+        for index in cells {
+            cells.move(fromOffsets: source, toOffset: destinstion)
+        }
     }
 }
 
